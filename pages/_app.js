fca1808645905936f36a44a17ac25677ef408e97
lichen404 <f360family@gmail.com>
2020-11-29 01:20:51 +0800

    Next.js 全解（上）

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
+import React from "react";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <div className="frank">
+            <Head>
+                <title>我的博客 - Frank</title>
+                <meta name="viewport"
+                      content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"/>
+            </Head>
+            <Component {...pageProps} />
+        </div>
+    )
+
 }
 
 export default MyApp
---
+++ pages/first-post.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default function X() {
+    return (
+        <div>First Post</div>
+    )
+}
