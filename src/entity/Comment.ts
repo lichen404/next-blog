47563e62fef7c65a416a5efdf64abfd1e8e4ca29
lichen404 <lichen@genew.com>
2020-12-21 14:58:33 +0800

    创建三个实体及其关联

---
+++ src/entity/Comment.ts
@@ -0,0 +1,16 @@
+import {Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import {User} from "./User";
+import {Post} from "./Post";
+
+@Entity()
+export class Comment {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+    @Column('text')
+    content: string;
+    @ManyToOne(type => User, user => user.comments)
+    user: User;
+    @ManyToOne(type => Post, post => post.comments)
+    post: Post;
+
+}
---
+++ src/entity/Post.ts
@@ -0,0 +1,30 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+import {User} from "./User";
+import {Comment} from "./Comment";
+import {comment} from "postcss";
+
+@Entity()
+export class Post {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+    @Column('varchar')
+    title: string;
+    @Column('text')
+    content: string;
+    @CreateDateColumn('time')
+    createdAt:Date;
+    @UpdateDateColumn('time')
+    updatedAt:Date;
+    @ManyToOne(type => User,user=>user.posts)
+    author:User;
+    @OneToMany(type=>Comment,comment=>comment.post)
+    comments:Comment[]
+}
